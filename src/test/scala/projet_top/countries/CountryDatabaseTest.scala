e3de87d777cd6e6f8fdbb4ba8e5a4ea063e9bc73
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2018-12-03 10:10:33 +0100

    add AirportDatabase.apply tests

--- src/test/scala/projet_top/countries/CountryDatabaseTest.scala
+++ src/test/scala/projet_top/countries/CountryDatabaseTest.scala
@@ -3,7 +3,7 @@ package projet_top.countries
 import org.scalatest.FlatSpec
 
 /**
-  * Tests pour la récupération par ID dans la AirportDatabase
+  * Tests pour la récupération par le nom dans la CountryDatabase
   */
 //noinspection ScalaFileName
 class CountryDatabaseGetByNameTest extends FlatSpec {
@@ -29,6 +29,40 @@ class CountryDatabaseGetByNameTest extends FlatSpec {
     val to_search = countryList(1)
 
     val found = countryDatabase.getCountryByName(to_search.countryName)
+
+    assert(found.countryName == to_search.countryName)
+    assert(found.inhabitants == to_search.inhabitants)
+    assert(found.surface == to_search.surface)
+  }
+}
+
+/**
+  * Tests pour la récupération par le nom via apply dans la CountryDatabase
+  */
+//noinspection ScalaFileName
+class CountryDatabaseApplyTest extends FlatSpec {
+  val countryList: List[Country] = Country (
+    countryName = "name 2",
+    inhabitants = 2,
+    surface = .2
+  ) :: Country (
+    countryName = "name 1",
+    inhabitants = 1,
+    surface = .1
+  ) :: Nil
+
+  val countryDatabase: CountryDatabase = CountryDatabase.fromList(countryList)
+
+  "A countryDatabase" should "throw an NSEE when looking for a non-existent name" in {
+    intercept[NoSuchElementException](
+      countryDatabase.apply("")
+    )
+  }
+
+  "A countryDatabase" should "return the correct country when looking for its name" in {
+    val to_search = countryList(1)
+
+    val found = countryDatabase.apply(to_search.countryName)
 
     assert(found.countryName == to_search.countryName)
     assert(found.inhabitants == to_search.inhabitants)
