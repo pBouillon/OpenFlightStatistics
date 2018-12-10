e2fd283aa830c849605c7b56e22182089b0b2a6f
vogt18u <florian.vogt1@etu.univ-lorraine.fr>
2018-12-10 11:23:32 +0100

    Amélioration de la lisibilité/des commentaires

--- src/main/scala/projet_top/airports/airport_filters/Hemisphere.scala
+++ src/main/scala/projet_top/airports/airport_filters/Hemisphere.scala
@@ -7,18 +7,17 @@ case object South extends HemisphereChoice
 
 case class Hemisphere(choice: HemisphereChoice) extends AirportFilter {
   /**
-    * Retourne true ssi l'aéroport est sur le bon hemisphère
-    * Si l'aéroport est sur l'équateur ou n'est pas sur l'hemisphère choisit, retourne false
+    * Teste si l'aéroport est sur l'hemisphère choisit
     *
     * @param candidate objet Airport à tester
     * @return true ssi l'aéroport est sur le bon hémisphère, false sinon
     */
   override def accepts(candidate: Airport): Boolean = choice match {
-      // Si on recherche les aéroports au nord, on retient ceux au dessus de l'equateur
+      // retient les aeroports au nord
     case North => candidate.latitude >= 0
-      // Si on recherche les aéroports au sud, on retient ceux en dessous de l'equateur
+      // retient les aeroports au sud
     case South => candidate.latitude <= 0
-      // Sinon, c'est une erreur ou un cas non traité
+      // cas non traité / erreur
     case _ => false
   }
 }
