8367f0855c34bd03f984f20102d6945206e547f0
Thomas BAGREL <tomsb07@gmail.com>
2018-12-13 20:28:45 +0100

    projection is on the road!

--- src/main/scala/projet_top/projections/ProjectedPoint.scala
+++ src/main/scala/projet_top/projections/ProjectedPoint.scala
@@ -2,5 +2,5 @@ package projet_top.projections
 
 sealed abstract class ProjectedPoint
 case object OutOfMap
-case class OnMap(x: Int, y: Int)(width: Int, height: Int)
+case class OnMap(x: Int, y: Int)
 
