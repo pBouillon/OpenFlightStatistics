c81d857915288c6b8280d02a94656fc6c54510da
axelthouvenin <axel.thouvenin@telecomnancy.eu>
2019-01-08 17:57:08 +0100

    modification sur test pour le fichier AirportDistanceMap

--- src/test/scala/projet_top/airport/AirportDistanceMapTest.scala
+++ src/test/scala/projet_top/airport/AirportDistanceMapTest.scala
@@ -2,8 +2,6 @@ package projet_top.airport
 
 import org.scalatest.FlatSpec
 
-
-
 object AirportDistanceMapTestObject {
 
   val airportDistanceMap = AirportDatabaseTestObjects.airportDatabase.getDistanceMap
@@ -53,31 +51,34 @@ class AirportDistanceMapGetDistanceBetweenTest extends FlatSpec {
     * Tests pour la récupération de la distance entre les deux aéroports choisis, identifiés par leur nom.
     */
   it should "throw an NSEE when looking for a non-existent airport name" in {
-
     val impossibleAirport = Airport(-1, "Gotham Aiport", "Gotham City", "Gotham Name", -1, 1)
 
     intercept[NoSuchElementException]{
       AirportDistanceMapTestObject.airportDistanceMap
-        .getDistanceBetween((impossibleAirport))(impossibleAirport)
+        .getDistanceBetween(impossibleAirport)(impossibleAirport)
     }
   }
+
   it should "throw an NSEE when looking for a non-existent airport name and an existent one" in {
     val impossibleAirport = Airport(-1, "Gotham Aiport", "Gotham City", "Gotham Name", -1, 1)
-    val possibleAirport = AirportDatabaseTestObjects.airportDatabase.getAirportById(1)
+    val possibleAirport = AirportDatabaseTestObjects.airportDatabase
+      .getAirportById(1)
 
     intercept[NoSuchElementException]{
       AirportDistanceMapTestObject.airportDistanceMap
-        .getDistanceBetween((possibleAirport))(impossibleAirport)
+        .getDistanceBetween(possibleAirport)(impossibleAirport)
     }
   }
 
   it should "throw a distance between airports in the map" in {
-    val possibleAirport1 = AirportDatabaseTestObjects.airportDatabase.getAirportById(1)
-    val possibleAirport2 = AirportDatabaseTestObjects.airportDatabase.getAirportById(2)
     val expected = 15.725331604303992
+    val possibleAirport1 = AirportDatabaseTestObjects.airportDatabase
+      .getAirportById(1)
+    val possibleAirport2 = AirportDatabaseTestObjects.airportDatabase
+      .getAirportById(2)
 
     val ret = AirportDistanceMapTestObject.airportDistanceMap
-        .getDistanceBetween((possibleAirport1))(possibleAirport2)
+        .getDistanceBetween(possibleAirport1)(possibleAirport2)
 
     assert(ret == expected)
   }
@@ -111,9 +112,9 @@ class AirportDistanceMapGetDistanceBetweenTest extends FlatSpec {
     }
 
     it should "throw a distance between airport in the map" in {
+      val expected = 15.725331604303992
       val validAirportId1 = 1
       val validAirportId2 = 2
-      val expected = 15.725331604303992
 
       val ret = AirportDistanceMapTestObject.airportDistanceMap.
         apply(validAirportId1)(validAirportId2)
@@ -126,27 +127,30 @@ class AirportDistanceMapGetDistanceBetweenTest extends FlatSpec {
 
       intercept[NoSuchElementException]{
         AirportDistanceMapTestObject.airportDistanceMap
-          .apply((impossibleAirport))(impossibleAirport)
+          .apply(impossibleAirport)(impossibleAirport)
       }
     }
 
     it should "throw an NSEE when looking for a non-existent airport name and an existent one" in {
       val impossibleAirport = Airport(-1, "Gotham Aiport", "Gotham City", "Gotham Name", -1, 1)
-      val possibleAirport = AirportDatabaseTestObjects.airportDatabase.getAirportById(1)
+      val possibleAirport = AirportDatabaseTestObjects.airportDatabase
+        .getAirportById(1)
 
       intercept[NoSuchElementException]{
         AirportDistanceMapTestObject.airportDistanceMap
-          .apply((possibleAirport))(impossibleAirport)
+          .apply(possibleAirport)(impossibleAirport)
       }
     }
 
     it should "throw a distance between airports in the map" in {
-      val possibleAirport1 = AirportDatabaseTestObjects.airportDatabase.getAirportById(1)
-      val possibleAirport2 = AirportDatabaseTestObjects.airportDatabase.getAirportById(2)
       val expected = 15.725331604303992
+      val possibleAirport1 = AirportDatabaseTestObjects.airportDatabase
+        .getAirportById(1)
+      val possibleAirport2 = AirportDatabaseTestObjects.airportDatabase
+        .getAirportById(2)
 
       val ret = AirportDistanceMapTestObject.airportDistanceMap
-        .apply((possibleAirport1))(possibleAirport2)
+        .apply(possibleAirport1)(possibleAirport2)
 
       assert(ret == expected)
     }
