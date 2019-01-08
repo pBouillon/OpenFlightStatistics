c4f715eb10e5775161dfae6d3f0b42b563df1286
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2019-01-08 23:52:11 +0100

    updated backmapprovider

--- src/main/scala/projet_top/cli/utils/MapCreation.scala
+++ src/main/scala/projet_top/cli/utils/MapCreation.scala
@@ -8,10 +8,22 @@ import projet_top.projection.backmap_providers.{BackmapProvider, D3BackmapProvid
 object MapCreation {
   // map provider
   val defaultBackmapProvider: BackmapProvider =
-    new D3BackmapProvider("http://d3backmapprovider.westeurope.azurecontainer.io:8080/")
+    new D3BackmapProvider("http://d3backmapprovider.westeurope.azurecontainer.io:8080/", style = Some(
+      """
+        |body {
+        |  background-color: #2b7f7f;
+        |}
+        |.country{
+        |  fill: #99c542;   /* country colour */
+        |  stroke: #aaaaaa; /* country border colour */
+        |  stroke-width: 1; /* country border width */
+        |}
+        |
+      """.stripMargin
+    ))
 
   // marker
-  var defaultMarkerColorValue: String = "#D34747"
+  val defaultMarkerColorValue: String = "#D34747"
   val defaultMarkerColor: Color = new Color (
     Integer.valueOf( defaultMarkerColorValue.substring( 1, 3 ), 16 ),
     Integer.valueOf( defaultMarkerColorValue.substring( 3, 5 ), 16 ),
